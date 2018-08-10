3a511c82a080772a8470539f41b4873f3fe55e7d
Heshik Nandan <heshik@oddle.me>
2018-08-10 17:22:58 +0800

    initial commit

---
+++ WebSpark/Models/DisplayBlock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace WebSpark.Models
+{
+    public class DisplayBlock
+    {
+        public DisplayBlock()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
